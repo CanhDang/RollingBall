c76fd9ddd35e5afdc66f5eb1ebd4318c5e1be068
HuuLuong <huuluong@techmasters-Mac-mini-2.local>
2016-07-19 14:45:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+RollingBall written by Dang Dinh Canh
+for iOS Swift Class 
+
+Making a ball running inside the screen 
--- RollingBall/ViewController.swift
+++ RollingBall/ViewController.swift
@@ -10,14 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var ball = UIImageView()
+    var ballRadius = CGFloat()
+    var radians = CGFloat()
+    var deltaAngle = CGFloat()
+    var moveX = CGFloat()
+    var moveY = CGFloat()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        ballRadius = 32
+        deltaAngle = 0.1
+        moveX = ballRadius * deltaAngle
+        moveY = ballRadius * deltaAngle
+        
+        addBackGround()
+        addBall()
+        
+        let time = NSTimer.scheduledTimerWithTimeInterval(0.02, target: self, selector: #selector(rollBall), userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addBackGround() -> Void {
+        let background = UIImageView(image: UIImage(named: "grass_yard.jpg"))
+        background.frame = self.view.bounds
+        background.contentMode = .ScaleAspectFill
+        self.view.addSubview((background))
+    }
+    
+    func addBall(){
+        
+        let mainViewSize = self.view.bounds.size
+       
+        ball = UIImageView(image: UIImage(named: "ball.png"))
+        ball.frame = CGRect(x: ballRadius, y: mainViewSize.height, width: ballRadius*2, height: ballRadius*2)
+        ball.center = CGPointMake(mainViewSize.width * 0.5, mainViewSize.height * 0.5)
+        
+        self.view.addSubview(ball)
+    }
+    
+    func rollBall() -> Void {
+        
+      
+        if (ball.center.x + ballRadius >= self.view.bounds.size.width) || (ball.center.x - ballRadius <= 0) {
+            radians = 0
+            deltaAngle = -deltaAngle
+            moveX = -moveX
+        }
+        if (ball.center.y + ballRadius >= self.view.bounds.size.height) || (ball.center.y - ballRadius <= 0) {
+            radians = 0
+            deltaAngle = -deltaAngle
+            moveY = -moveY
+        }
+        
+        radians = radians + deltaAngle
+        ball.transform = CGAffineTransformMakeRotation(radians)
+        ball.center = CGPointMake(ball.center.x + moveX, ball.center.y + moveY)
+        
+        
     }
 
 
